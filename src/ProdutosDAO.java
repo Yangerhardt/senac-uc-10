e39ecddb402fa80e049c4f208de57c4c388a6e71
Yan Krug Gerhardt <yan-ragna@hotmail.com>
2024-02-21 17:52:49 -0300

    adding function to sell product

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -2,6 +2,7 @@ import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 
@@ -22,5 +23,21 @@ public class ProdutosDAO {
         
         return listagem;
     }
+    
+    public void venderProduto(int idProduto) {
+    try {
+        conn = new conectaDAO().connectDB();
+        
+        String sql = "UPDATE tabela_produtos SET status = 'Vendido' WHERE id = ?";
+        prep = conn.prepareStatement(sql);
+        prep.setInt(1, idProduto);
+        
+        prep.executeUpdate();
+        
+        JOptionPane.showMessageDialog(null, "Produto vendido com sucesso!");
+    } catch (SQLException erro) {
+        JOptionPane.showMessageDialog(null, "Erro ao vender produto: " + erro.getMessage());
+    }
+}
 }
 
